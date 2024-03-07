b15abd0a3b01179ce2199e0ed80b060f8e36927e
Touficoo <106972096+Touficoo@users.noreply.github.com>
2024-03-07 11:33:25 +0200

    Write a program that prints _putchar, followed by a new line.

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,15 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    const char str[] = "_putchar\n";
-    size_t len = sizeof(str) - 1;
-    write(STDOUT_FILENO, str, len);
-    return 0;
+	const char str[] = "_putchar\n";
+	size_t len = sizeof(str) - 1;
+
+	write(STDOUT_FILENO, str, len);
+	return (0);
 }
