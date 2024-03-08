6045d27aeb7df0cdf2eea40265781e163c471a9e
Touficoo <106972096+Touficoo@users.noreply.github.com>
2024-03-08 21:30:56 +0200

    Write a function that checks for a digit (0 through 9).

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: the character to check
- *
- * Return: 1 if c is uppercase, 0 otherwise
- */
-int _isdigit(int c)
+  * _isdigit - Check if a character is a digit
+  * @x: The number to be checked
+  *
+  * Return: 1 for a character that will be a digit or 0 for any else
+  */
+int _isdigit(int x)
 {
-	return (c >= 'A' && c <= 'Z');
+	if (x >= 48 && x <= 57)
+	{
+		return (1);
+	}
+
+	return (0);
 }
