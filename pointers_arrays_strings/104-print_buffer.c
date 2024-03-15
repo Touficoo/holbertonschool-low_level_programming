e45d5313be4102a148e2e178c0552d4181a0f718
Touficoo <106972096+Touficoo@users.noreply.github.com>
2024-03-16 00:29:17 +0200

    Write a function that prints a buffer.

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -11,20 +11,20 @@ void print_buffer(char *b, int size)
 {
 int i, j;
 
-if (size <= 0)
+    if (size <= 0)
 {
     printf("\n");
     return;
 }
 
-for (i = 0; i < size; i += 10)
+    for (i = 0; i < size; i += 10)
 {
     printf("%08x: ", i);
 
-    for (j = 0; j < 10; j++)
+        for (j = 0; j < 10; j++)
         {
             if (i + j < size)
-                printf("%02x", (unsigned char)b[i + j]);
+            printf("%02x", (unsigned char)b[i + j]);
             else
                 printf("  ");
 
