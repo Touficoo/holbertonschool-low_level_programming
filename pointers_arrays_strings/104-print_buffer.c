7e5ba2b3a31d893a1fa5ec72f8d1f19d5e18feff
Touficoo <106972096+Touficoo@users.noreply.github.com>
2024-03-16 00:31:14 +0200

    Write a function that prints a buffer.

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -9,40 +9,40 @@
  */
 void print_buffer(char *b, int size)
 {
-int i, j;
+	int i, j;
 
-    if (size <= 0)
-{
-    printf("\n");
-    return;
-}
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
 
-    for (i = 0; i < size; i += 10)
-{
-    printf("%08x: ", i);
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
 
-        for (j = 0; j < 10; j++)
-        {
-            if (i + j < size)
-            printf("%02x", (unsigned char)b[i + j]);
-            else
-                printf("  ");
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				printf("%02x", (unsigned char)b[i + j]);
+			else
+				printf("  ");
 
-            if (j % 2 == 1)
-                printf(" ");
-        }
+			if (j % 2 == 1)
+				printf(" ");
+		}
 
-        for (j = 0; j < 10; j++)
-        {
-            if (i + j < size)
-            {
-                if (isprint(b[i + j]))
-                    printf("%c", b[i + j]);
-                else
-                    printf(".");
-            }
-        }
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				if (isprint(b[i + j]))
+					printf("%c", b[i + j]);
+				else
+					printf(".");
+			}
+		}
 
-        printf("\n");
-    }
+		printf("\n");
+	}
 }
